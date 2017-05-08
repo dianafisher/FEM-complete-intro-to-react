4c3249ea6555dd40338c3addc2eeee7efefe2efc
Diana Fisher <diana.fisher@gmail.com>
2017-05-08 14:24:42 -0700

    Install standard

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,9 +1,10 @@
+/* global React ReactDOM */
 
 var MyTitle = React.createClass({
   render: function () {
     return (
       React.DOM.div(null,
-        React.DOM.h1({ style: { color: this.props.color, fontWeight: 'bold' }}, this.props.title))
+        React.DOM.h1({ style: { color: this.props.color, fontWeight: 'bold' } }, this.props.title))
     )
   }
 })
@@ -11,12 +12,12 @@ var MyTitle = React.createClass({
 var MyTitleFactory = React.createFactory(MyTitle)
 
 var MyFirstComponent = React.createClass({
-  render: function() {
+  render: function () {
     return (
       React.DOM.div(null,
-        MyTitleFactory({ title: 'props are the best', color: 'peru'}),
-        MyTitleFactory({ title: 'semicolons are the worst', color: 'mediumaquamarine'}),
-        MyTitleFactory({ title: 'jklol its okay if you like semicolons', color: 'blanchedalmond'}),
+        MyTitleFactory({ title: 'props are the best', color: 'peru' }),
+        MyTitleFactory({ title: 'semicolons are the worst', color: 'mediumaquamarine' }),
+        MyTitleFactory({ title: 'jklol its okay if you like semicolons', color: 'blanchedalmond' }),
         MyTitleFactory({ title: 'i am out of ideas', color: 'rebeccapurple' }))
         // React.DOM.h1(null, 'This is my first component!'))
     )
