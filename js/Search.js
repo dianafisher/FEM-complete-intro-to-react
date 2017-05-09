83e7767ef0ebfa178e4c639b26e51fd316ea2b1f
Diana Fisher <diana.fisher@gmail.com>
2017-05-09 12:08:21 -0700

    Create a ShowCard component for each show.

--- js/Search.js
+++ js/Search.js
@@ -1,13 +1,14 @@
 import React from 'react'
+import ShowCard from './ShowCard'
 import preload from '../public/data.json'
 
 const Search = React.createClass({
   render () {
     return (
       <div className='search'>
-        {preload.shows.map(function (show) {
+        {preload.shows.map((show) => {
           return (
-            <h3>{show.title}</h3>
+            <ShowCard show={show} key={show.imdbID}></ShowCard>
           )
         })}
       </div>
