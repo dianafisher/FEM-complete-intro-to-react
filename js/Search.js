37047ee9d368ac921183ffd244fd2685a167e159
Diana Fisher <diana.fisher@gmail.com>
2017-05-09 14:53:43 -0700

    Update state as user types in search input

--- js/Search.js
+++ js/Search.js
@@ -4,16 +4,35 @@ import preload from '../public/data.json'
 
 const Search = React.createClass({
 
+  getInitialState () {
+    return {
+      searchTerm: 'I am a random string.'
+    }
+  },
+  handleSearchTermChange (event) {
+    this.setState({searchTerm: event.target.value})
+  },
+
   render () {
     return (
       <div className='search'>
-        {preload.shows.map((show) => {
-          return (
-            <ShowCard show={show} key={show.imdbID} />
-          )
-        })}
+        <header>
+          <h1>svideo</h1>
+          <input
+            value={this.state.searchTerm}
+            onChange={this.handleSearchTermChange}
+            type='text'
+            placeholder='Search' />
+        </header>
+        <div>
+          {preload.shows.map((show) => {
+            return (
+              <ShowCard show={show} key={show.imdbID} />
+            )
+          })}
+        </div>
       </div>
-    )
+      )
   }
 })
 
