0c699b34f46e8f9b0981c9bfe2e1b4a82e31a616
Diana Fisher <diana.fisher@gmail.com>
2017-05-10 16:05:21 -0700

    Handle change event on input.

--- js/Landing.js
+++ js/Landing.js
@@ -1,16 +1,40 @@
 import React from 'react'
+import { connect } from 'react-redux'
 import { Link } from 'react-router'
+import { setSearchTerm } from './actionCreators'
+const { string, func } = React.PropTypes
 
 const Landing = React.createClass({
+  propTypes: {
+    searchTerm: string,
+    dispatch: func
+  },
+  handleSearchTermChange (event) {
+    // dispatch comes from connect
+    // pass the value to our action creator
+    this.props.dispatch(setSearchTerm(event.target.value))
+  },
   render () {
     return (
       <div className='landing'>
         <h1>svideo</h1>
-        <input type='text' placeholder='Search' />
+        <input
+          value={this.props.searchTerm}
+          onChange={this.handleSearchTermChange}
+          type='text'
+          placeholder='Search' />
         <Link to='/search'>or Browse All</Link>
       </div>
     )
   }
 })
 
-export default Landing
+// searchTerm is now a prop available to Landing
+const mapStateToProps = (state) => {
+  return {
+    searchTerm: state.searchTerm
+  }
+}
+
+// Landing is now a redux-connected component
+export default connect(mapStateToProps)(Landing)
