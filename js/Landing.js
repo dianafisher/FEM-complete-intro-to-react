e2c656e1dc1d4a8cbccfb0f979c74f4c9910c6b2
Diana Fisher <diana.fisher@gmail.com>
2017-05-10 16:27:41 -0700

    Go to search page from landing page.

--- js/Landing.js
+++ js/Landing.js
@@ -2,9 +2,12 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { Link } from 'react-router'
 import { setSearchTerm } from './actionCreators'
-const { string, func } = React.PropTypes
+const { string, func, object } = React.PropTypes
 
 const Landing = React.createClass({
+  contextTypes: {
+    router: object
+  },
   propTypes: {
     searchTerm: string,
     dispatch: func
@@ -14,15 +17,23 @@ const Landing = React.createClass({
     // pass the value to our action creator
     this.props.dispatch(setSearchTerm(event.target.value))
   },
+  handleSearchSubmit (event) {
+    event.preventDefault()
+    // go to the search page programatically via react router
+    this.context.router.transitionTo('/search')
+  },
   render () {
     return (
       <div className='landing'>
         <h1>svideo</h1>
-        <input
-          value={this.props.searchTerm}
-          onChange={this.handleSearchTermChange}
-          type='text'
-          placeholder='Search' />
+        <form onSubmit={this.handleSearchSubmit}>
+          <input
+            value={this.props.searchTerm}
+            onChange={this.handleSearchTermChange}
+            type='text'
+            placeholder='Search'
+          />
+          </form>
         <Link to='/search'>or Browse All</Link>
       </div>
     )
