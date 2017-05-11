e7791ce1765fc5435bba458945077742ae9bb8e2
Diana Fisher <diana.fisher@gmail.com>
2017-05-10 20:15:37 -0700

    Modify store to enable redux dev tools.

--- js/store.js
+++ js/store.js
@@ -1,6 +1,11 @@
-import { createStore } from 'redux'
+import { createStore, compose } from 'redux'
 import rootReducer from './reducers'
 
-const store = createStore(rootReducer)
+const store = createStore(rootReducer, compose(
+  /*
+    If we are in the browser and have devToolsExtension, turn on devToolsExtension.  Else, to nothing.
+  */
+  typeof window === 'object' && typeof window.devToolsExtension !== 'undefined' ? window.devToolsExtension() : (f) => f
+))
 
 export default store
